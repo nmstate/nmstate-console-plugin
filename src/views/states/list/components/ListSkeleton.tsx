af5c52d8110b16949e92802293bdb72cee28a91a
Ugo Palatucci <upalatuc@redhat.com>
2023-06-14 10:36:21 +0200

    fix: states list loading state

--- src/views/states/list/components/ListSkeleton.tsx
+++ src/views/states/list/components/ListSkeleton.tsx
@@ -1,39 +1,30 @@
 import React from 'react';
-import NodeNetworkStateModel from 'src/console-models/NodeNetworkStateModel';
 
-import { ListPageBody, ListPageHeader } from '@openshift-console/dynamic-plugin-sdk';
 import { Flex, Skeleton, Stack, StackItem } from '@patternfly/react-core';
-import { useNMStateTranslation } from '@utils/hooks/useNMStateTranslation';
 
-const LinkSkeleton: React.FC = () => {
-  const { t } = useNMStateTranslation();
-  return (
-    <div>
-      <ListPageHeader title={t(NodeNetworkStateModel.label)}></ListPageHeader>
-      <ListPageBody>
-        <Stack hasGutter>
-          <Flex justifyContent={{ default: 'justifyContentSpaceBetween' }}>
-            <Skeleton width="30%" fontSize="2xl" />
-            <Skeleton width="30%" fontSize="2xl" />
-            <Skeleton width="30%" fontSize="2xl" />
-          </Flex>
+const LinkSkeleton: React.FC = () => (
+  <div>
+    <Stack hasGutter>
+      <Flex justifyContent={{ default: 'justifyContentSpaceBetween' }}>
+        <Skeleton width="30%" fontSize="2xl" />
+        <Skeleton width="30%" fontSize="2xl" />
+        <Skeleton width="30%" fontSize="2xl" />
+      </Flex>
 
-          <StackItem>
-            <Skeleton fontSize="lg" />
-          </StackItem>
-          <StackItem>
-            <Skeleton fontSize="lg" />
-          </StackItem>
-          <StackItem>
-            <Skeleton fontSize="lg" />
-          </StackItem>
-          <StackItem>
-            <Skeleton fontSize="lg" />
-          </StackItem>
-        </Stack>
-      </ListPageBody>
-    </div>
-  );
-};
+      <StackItem>
+        <Skeleton fontSize="lg" />
+      </StackItem>
+      <StackItem>
+        <Skeleton fontSize="lg" />
+      </StackItem>
+      <StackItem>
+        <Skeleton fontSize="lg" />
+      </StackItem>
+      <StackItem>
+        <Skeleton fontSize="lg" />
+      </StackItem>
+    </Stack>
+  </div>
+);
 
 export default LinkSkeleton;
