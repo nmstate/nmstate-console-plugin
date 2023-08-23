42f81b33795feef3b85e11e549bd5e272c627099
Ugo Palatucci <upalatuc@redhat.com>
2023-08-23 09:52:09 +0200

    dns-resolver policy details page

--- src/views/policies/details/PolicyDetailsPage.tsx
+++ src/views/policies/details/PolicyDetailsPage.tsx
@@ -25,6 +25,8 @@ const PolicyDetailsPage: FC<PolicyDetailsPageProps> = ({ obj: policy }) => {
   const firstInterface = policy?.spec?.desiredState
     ?.interfaces?.[0] as NodeNetworkConfigurationInterface;
 
+  const dnsResolver = policy?.spec?.desiredState?.['dns-resolver'];
+
   const description = policy?.metadata?.annotations?.['description'];
 
   return (
@@ -38,26 +40,54 @@ const PolicyDetailsPage: FC<PolicyDetailsPageProps> = ({ obj: policy }) => {
             <DescriptionListTerm>{t('Name')}</DescriptionListTerm>
             <DescriptionListDescription>{policy?.metadata?.name}</DescriptionListDescription>
           </DescriptionListGroup>
-          <DescriptionListGroup>
-            <DescriptionListTerm>{t('Description')}</DescriptionListTerm>
-            <DescriptionListDescription>{description}</DescriptionListDescription>
-          </DescriptionListGroup>
-          <DescriptionListGroup>
-            <DescriptionListTerm>{t('Type')}</DescriptionListTerm>
-            <DescriptionListDescription>{firstInterface?.type}</DescriptionListDescription>
-          </DescriptionListGroup>
-          <DescriptionListGroup>
-            <DescriptionListTerm>{t('Network state')}</DescriptionListTerm>
-            <DescriptionListDescription>{firstInterface?.state}</DescriptionListDescription>
-          </DescriptionListGroup>
-          <DescriptionListGroup>
-            <DescriptionListTerm>{t('IP configuration')}</DescriptionListTerm>
-            <DescriptionListDescription>
-              {!!firstInterface?.ipv4 && t('IPv4')}
-              {!!firstInterface?.ipv6 && !firstInterface?.ipv4 && t('IPv6')}
-              {!firstInterface?.ipv6 && !firstInterface?.ipv4 && t('None')}
-            </DescriptionListDescription>
-          </DescriptionListGroup>
+          {description && (
+            <DescriptionListGroup>
+              <DescriptionListTerm>{t('Description')}</DescriptionListTerm>
+              <DescriptionListDescription>{description}</DescriptionListDescription>
+            </DescriptionListGroup>
+          )}
+          {firstInterface && (
+            <>
+              <DescriptionListGroup>
+                <DescriptionListTerm>{t('Type')}</DescriptionListTerm>
+                <DescriptionListDescription>{firstInterface?.type}</DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTerm>{t('Network state')}</DescriptionListTerm>
+                <DescriptionListDescription>{firstInterface?.state}</DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTerm>{t('IP configuration')}</DescriptionListTerm>
+                <DescriptionListDescription>
+                  {!!firstInterface?.ipv4 && t('IPv4')}
+                  {!!firstInterface?.ipv6 && !firstInterface?.ipv4 && t('IPv6')}
+                  {!firstInterface?.ipv6 && !firstInterface?.ipv4 && t('None')}
+                </DescriptionListDescription>
+              </DescriptionListGroup>
+            </>
+          )}
+
+          {dnsResolver?.config?.search && (
+            <>
+              <DescriptionListGroup>
+                <DescriptionListTerm>{t('Search')}</DescriptionListTerm>
+                <DescriptionListDescription>
+                  {dnsResolver?.config?.search.join(', ')}
+                </DescriptionListDescription>
+              </DescriptionListGroup>
+            </>
+          )}
+
+          {dnsResolver?.config?.server && (
+            <>
+              <DescriptionListGroup>
+                <DescriptionListTerm>{t('Server')}</DescriptionListTerm>
+                <DescriptionListDescription>
+                  {dnsResolver?.config?.server.join(', ')}
+                </DescriptionListDescription>
+              </DescriptionListGroup>
+            </>
+          )}
         </DescriptionList>
       </PageSection>
     </div>
